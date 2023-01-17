7d56d60d2e4367e6c820ecdc36bfae1cde7ba0ca
Patricia Tebar <7901494@alumnos.ufv.es>
2023-01-17 18:36:53 +0100

    se ha subido el front y el back

---
+++ ptb/ptb/src/main/java/ufv/extraordinaria/dis/ptb/PtbApplication.java
@@ -0,0 +1,13 @@
+package ufv.extraordinaria.dis.ptb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtbApplication.class, args);
+	}
+
+}
